0b2afaf63c53ce138f4264ae9932f96a71865c8b
Stuart Elimu <stuartelimu@gmail.com>
2022-04-11 18:36:45 +0300

    setup mongodb

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+// import mongoose module
+const mongoose = require('mongoose');
+
+// setup mongoose connection
+const mongoDB = '127.0.0.1'
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// get default connection
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
