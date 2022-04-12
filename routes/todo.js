923f6933d040b840e3b09b22ff31e575da9ee96a
Stuart Elimu <stuartelimu@gmail.com>
2022-04-12 17:53:18 +0300

    fix bug in routes

--- routes/todo.js
+++ routes/todo.js
@@ -9,5 +9,7 @@ router.get('/', todo_controller.todo_list);
 router.get('/todos', todo_controller.todo_list);
 router.get('/todo/:todoId', todo_controller.todo_detail);
 router.post('/todo/create', todo_controller.todo_create);
-router.put('/todo:todoId', todo_controller.todo_update);
+router.put('/todo/:todoId', todo_controller.todo_update);
 router.delete('/todo/:todoId', todo_controller.todo_delete);
+
+module.exports = router;
